23bd20fd8d1141b0ecd50d6c4163ae17f3ff51ac
yuyu <sishichulu@gmail.com>
2025-01-20 14:00:17 +0300

    feat: completed 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, 'utf8');
+readStream.pipe(process.stdout);
+
+readStream.on('error', (err) => {
+    console.log("Error: ", error.message);
+});
